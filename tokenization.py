6f73819c070f3698814ac4b058b00c7840834267
Akiva Miura <akiva.miura@gmail.com>
2023-06-07 19:56:16 +0900

    トークンのオフセット合わせをNFKDベースで再実装

--- tokenization.py
+++ tokenization.py
@@ -12,12 +12,9 @@ from dataclasses import (
     asdict,
     dataclass,
 )
-import pandas as pd
-import transformers
 from transformers import (
     AutoTokenizer,
-    PreTrainedTokenizer,
-    PreTrainedTokenizerFast,
+    BertJapaneseTokenizer,
 )
 
 from logzero import logger
@@ -35,47 +32,10 @@ class TokenWithOffset:
     end_line: int
     end_offset: int
 
-def normalize(text):
-    # Unicode正規化 
-    return unicodedata.normalize('NFKC', text)
-
-def merge_tokens(tokens: list[TokenWithOffset]):
-    # トークンを結合して文字列を生成する
-    #return normalize( str.join('', [token.text for token in tokens]) )
-    return str.join('', [token.text for token in tokens])
-
-def decompose_line(line, line_index):
-    # ライン文字列を構成文字単位でトークナイズする
-    # (TRADE_MARK_SIGNのような合字をサブワード化された '##T' などとマッチさせるための対策)
-    traceable_chars: list[TokenWithOffset] = []
-    for i, c in enumerate(line):
-        assert len(c) == 1
-        if c in [' ', '\t', '　']:
-            continue
-        sub_chars = normalize(c)
-        for sub_char in sub_chars:
-            # 合字の場合は1文字ずつ、元の合字の開始位置と終了位置を記録する
-            # 合字でなければそのままトークン化する
-            if sub_char in [' ']:
-                # 稀にノーマライズすると空白文字が入っている場合がある
-                # 例: ACUTE ACCENT -> SPACE + COMBINING ACUTE ACCENT
-                continue
-            traceable_char = TokenWithOffset(
-                text=sub_char,
-                start_line=line_index,
-                start_offset=i,
-                end_line=line_index,
-                end_offset=i+1,
-            )
-            traceable_chars.append(traceable_char)
-    return traceable_chars
-
 def log_error_info(
     char_index = None,
     line = None,
-    line_chars = None,
     line_index = None,
-    merged = None,
     tokens = None,
     token_index = None,
     traceable_tokens = None,
@@ -89,7 +49,6 @@ def log_error_info(
     word = get_index(words, word_index)
     token = get_index(tokens, token_index)
     logger.error('line_index: %s', line_index)
-    #logger.error('line: %s', line)
     logger.error('char_index: %s', char_index)
     logger.error('word: %s', word)
     if word is not None:
@@ -102,117 +61,93 @@ def log_error_info(
         for i, c in enumerate(token):
             logger.error('token[%s]: %s', i, c)
             logger.error('token[%s] name: %s', i, unicodedata.name(c))
-        normal = normalize(token)
-        for i, c in enumerate(normal):
+        kd_token = unicodedata.normalize('NFKD', token)
+        for i, c in enumerate(kd_token):
             logger.error('normal token[%s]: %s', i, c)
             logger.error('normal token[%s] name: %s', i, unicodedata.name(c))
         logger.error('words digest: %s', words[word_index-5:word_index+5])
     logger.error('tokens digest: %s', tokens[token_index-5:token_index+5])
-    if line_chars is not None and char_index is not None:
-        for o in range(char_index-2, char_index+3):
-            if o >= 0 and o < len(line_chars):
-                logger.error('line character %s: %s', o, line_chars[o])
-                name = unicodedata.name(line_chars[o].text)
-                logger.error('line character %s name: %s', o, name)
-    if merged is not None:
-        logger.error('merged: %s', merged)
+    if line is not None and char_index is not None:
+        logger.error('offset character: %s', line[char_index])
+        name = unicodedata.name(line[char_index])
+        logger.error('offset character name: %s', name)
+        logger.error('line digest: %s', line[char_index-5:char_index+5])
     if traceable_tokens is not None:
         for i in range(-5, 0):
             logger.error('traceable token %s: %s', i, traceable_tokens[i])
 
 def tokenize_with_offsets(
-    tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast,
+    tokenizer: BertJapaneseTokenizer,
     text,
 ) -> list[TokenWithOffset]:
     # トークナイズした上で元のポジション位置を突き合わせる
     lines = text.splitlines()
     traceable_tokens = []
+
+    # 取り急ぎ、サブワードトークナイザに wordpiece を想定
+    # 他のサブワードトークナイザに対応させるのも比較的用意だが、
+    # サブワードの仕様に対して個別に対応する必用あり
     if tokenizer.subword_tokenizer_type != 'wordpiece':
         raise ValueError(f'unsupported subword tokenizer: {tokenizer.subword_tokenizer_type}')
     
     for line_index, line in enumerate(lines):
         #if DEBUG_MODE and line_index > 5:
         #    break
-        line_chars = decompose_line(line, line_index)
         words = tokenizer.word_tokenizer.tokenize(line)
         char_index = 0
-        last_token = None
-        unk_start_index = None
+        pending: list[str] = []
         for word_index, word in enumerate(words):
             #logger.debug('word %s: %s', word_index, word)
             tokens = tokenizer.subword_tokenizer.tokenize(word)
             if tokens == [tokenizer.unk_token]:
-                if len(word) >= 2:
-                    # 複数文字で構成される単語が未知語となった場合は
-                    # 1文字ずつトークン化する
-                    #logger.debug('tokens: %s', tokens)
-                    tokens = []
-                    for c in word:
-                        tokens.extend(tokenizer.subword_tokenizer.tokenize(c))
-                        #tokens.extend(c)
-                    #logger.debug('new tokens: %s', tokens)
+                # サブワードトークナイザで単一の未知語になった場合は、
+                # word 単体が未知語であったため、文字単位に分解する
+                tokens = list(word)
             for token_index, token in enumerate(tokens):
                 #logger.debug('token %s: %s', token_index, token)
-                if token is tokenizer.unk_token:
-                    if last_token is tokenizer.unk_token:
-                        # 連続する UNK は1つの UNK とみなす
-                        continue
-                    #unk_start_offset = offset
-                    unk_start_index = char_index
-                    # UNK トークンは実際には元の文内の文字に対応していない場合もある
-                    # 次の有効トークンまでスキップ
-                    last_token = token
-                    continue
-                #logger.debug('token: %s', token)
-                actual_token = token
+                actual_token = token.strip()
                 if token_index >= 1:
                     # サブワードの '##' は2番目以降のサブワードにしか付与されない
                     if token.startswith('##'):
-                        actual_token = token[2:]
+                        actual_token = actual_token[2:]
                 #logger.debug('actual token: %s', actual_token)
-                normal_token = normalize(actual_token)
-                first_char = normal_token[0]
-                found_first_char = False
-                #for first_char_offset in range(offset, len(line_chars)):
+                kd_token = unicodedata.normalize('NFKD', actual_token)
+                first_char = kd_token[0]
+                first_char_category = unicodedata.category(first_char)
+                if first_char_category == 'Mn':
+                    if len(token) == 1:
+                        # NOTE: 幅なし結合文字単体のトークンは非常に厄介
+                        # 正規化すると順序が変わる場合があるため、オフセット合わせが困難
+                        # 例 (順序正規化, canonical ordering):
+                        # normalize('NFD', COMBINING DIAERESIS' + 'COMBINING BREVE BELOW' + ...)
+                        # -> 'COMBINING BREVE BELOW' + 'COMBINING DIAERESIS' + ...
+                        # 対策: pending に溜めておいて、有効な文字が出てきたら一気に処理する
+                        pending.append(token)
+                        continue
+                if token not in tokenizer.subword_tokenizer.vocab:
+                    # 未知語は後から辻褄合わせをする
+                    pending.append(token)
+                    continue
+                found_first_char = -1
                 # 最初の1文字を探す
-                #for first_char_index in range(char_index, len(line_chars)):
-                for test_end in range(char_index, len(line_chars)):
+                for test_index in range(char_index, len(line)):
                     # NOTE: 入力文は normalize されていない場合が多々ある
-                    # line_chars の各文字には normalize で結合される前の文字が入っている場合がある
+                    # line の各文字には正規化で結合される前の文字が入っている場合がある
                     # 例: 'ザ' の代わりに
-                    # 'KATAKANA LETTER SA' + 'COMBINING KATAKANA=HIRAGANA VOICED SOUND MARK'
+                    # 'KATAKANA LETTER SA' + 'COMBINING KATAKANA-HIRAGANA VOICED SOUND MARK'
                     # の 2 文字が入っているなど
-                    # 対策: まずは終了位置を広げながら結合して first_char が含まれる位置を探す
-                    # 次に、開始位置を狭めながら first_char が含まれなくなる位置を探す
-                    # first_char が含まれない開始位置の1つ前が first_char の開始位置として適切
-                    merged = merge_tokens(line_chars[char_index:test_end+1])
-                    normal_merged = normalize(merged)
-                    #if first_char == unicodedata.lookup('COMBINING ACUTE ACCENT'):
-                    #    logger.warning('merged: %s', merged)
-                    if first_char in merged or first_char in normal_merged:
-                        #logger.debug('found first char %s in merged: %s', first_char, merged)
-                        # 開始位置の探索
-                        for test_start in range(
-                            char_index+1,
-                            test_end+2,
-                        ):
-                            merged = merge_tokens(
-                                line_chars[test_start:test_end+1]
-                            )
-                            normal_merged = normalize(merged)
-                            #if first_char not in merged:
-                            if first_char not in merged and first_char not in normal_merged:
-                                first_char_start_index = test_start - 1
-                                found_first_char = True
-                                break
+                    # 対策: まずは first_char を構成文字に含む文字を探す
+                    kd_char = unicodedata.normalize('NFKD', line[test_index])
+                    found_first_char = kd_char.find(first_char)
+                    if found_first_char >= 0:
+                        first_char_start_index = test_index
                         break
-                if not found_first_char:
-                    #logger.debug('short_merged: %s', memo_short_merged)
-                    # 1文字目が見つからない場合はエラー
+                if found_first_char < 0:
+                    # NOTE: 1文字目が見つからない場合はエラー
                     logger.error('first_char not found: %s', first_char)
+                    logger.error('first char name: %s', unicodedata.name(first_char))
                     log_error_info(
                         char_index=char_index,
-                        line_chars=line_chars,
                         line=line,
                         line_index=line_index,
                         tokens=tokens,
@@ -222,50 +157,47 @@ def tokenize_with_offsets(
                         word_index=word_index,
                     )
                     raise ValueError(f'first char not found: {first_char}')
-                if last_token is tokenizer.unk_token:
-                    unk_start_token = line_chars[unk_start_index]
-                    first_char_token = line_chars[first_char_start_index]
-                    if unk_start_token.start_offset == first_char_token.start_offset:
-                        # NOTE: UNK は元の文字に対応していない
-                        # 空文字列のトークンは不要であり問題が起こるため無視
-                        #logger.debug('empty unk token before: %s', first_char_token)
-                        pass
-                    else:
+                if len(pending) > 0:
+                    # NOTE: オフセット合わせできていなかったトークンの辻褄を合わせる
+                    for p in pending:
+                        if first_char_start_index - char_index == 0:
+                            if p not in tokenizer.subword_tokenizer.vocab:
+                                # NOTE: 長さ0の未知語を出力する必用は無い
+                                continue
                         traceable_token = TokenWithOffset(
-                            text=merge_tokens(line_chars[unk_start_index:first_char_start_index]),
-                            start_line=unk_start_token.start_line,
-                            start_offset=unk_start_token.start_offset,
-                            end_line=first_char_token.end_line,
-                            # 最初の有効文字の start_offset が UNK の end_offset になる
-                            end_offset=first_char_token.start_offset,
+                            text=p,
+                            start_line=line_index,
+                            start_offset=char_index,
+                            end_line=line_index,
+                            end_offset=first_char_start_index,
                         )
                         traceable_tokens.append(traceable_token)
-                        #logger.debug('unk token: %s', traceable_token)
-                char_index = first_char_start_index
+                    pending.clear()
                 matched = False
-                merged = None
-                for test_end in range(char_index, len(line_chars)):
-                    merged = merge_tokens(line_chars[char_index:test_end+1])
-                    normal_merged = normalize(merged)
-                    #if DEBUG_MODE:
-                    #    logger.debug('char_index: %s', char_index)
-                    #    logger.debug('end_char_index: %s', end_char_index)
-                    #    logger.debug('token: %s', token)
-                    #    logger.debug('normal_token: %s', normal_token)
-                    #    logger.debug('merged: %s', merged)
-                    #if normal_token == merged:
-                    if normal_token == normal_merged:
+                for test_end in range(first_char_start_index+1, len(line)+1):
+                    span = line[first_char_start_index:test_end]
+                    kd_span = unicodedata.normalize('NFKD', span)
+                    kd_span = kd_span[found_first_char:]
+                    if kd_token == kd_span:
+                        # NOTE: 完全一致
+                        # 次の文字にオフセットを移して問題無い
                         matched = True
                         end_char_index = test_end
+                        next_char_index = test_end
+                        break
+                    if kd_span.startswith(kd_token):
+                        # NOTE: 前方部分一致
+                        # 次に来るトークンが最後の文字の構成要素の可能性もある
+                        matched = True
+                        end_char_index = test_end
+                        next_char_index = test_end - 1
                         break
                 if not matched:
                     logger.error('token not found: %s', token)
                     log_error_info(
-                        char_index=char_index,
-                        line_chars=line_chars,
+                        char_index=first_char_start_index,
                         line=line,
                         line_index=line_index,
-                        merged=merged,
                         tokens=tokens,
                         token_index=token_index,
                         traceable_tokens=traceable_tokens,
@@ -273,49 +205,44 @@ def tokenize_with_offsets(
                         word_index=word_index,
                     )
                     raise ValueError('token not found')
-                start_token = line_chars[char_index]
-                end_token = line_chars[end_char_index]
                 traceable_token = TokenWithOffset(
                     text=token,
-                    start_line=start_token.start_line,
-                    start_offset=start_token.start_offset,
-                    end_line=end_token.end_line,
-                    end_offset=end_token.end_offset,
+                    start_line=line_index,
+                    start_offset=first_char_start_index,
+                    end_line=line_index,
+                    end_offset=end_char_index,
                 )
                 traceable_tokens.append(traceable_token)
-                char_index = end_char_index + 1
-                last_token = token
-        if last_token is tokenizer.unk_token:
-            # 行末の UNK トークン
-            unk_start_token = line_chars[unk_start_index]
-            end_token = line_chars[-1]
-            if unk_start_token.start_offset == end_token.start_offset:
-                # NOTE: UNK は元の文字に対応していない
-                # 空文字列のトークンは不要であり問題が起こるため無視
-                #logger.debug('empty unk token at line end')
-                pass
-            else:
-                #start_token = line_chars[unk_start_offset]
-                #end_token = line_chars[offset]
+                char_index = next_char_index
+        if len(pending) > 0:
+            # NOTE: オフセット合わせできていなかったトークンの辻褄を合わせる
+            for p in pending:
+                end_index = len(line)
+                if end_index - char_index == 0:
+                    if p not in tokenizer.subword_tokenizer:
+                        # NOTE: 長さ0の未知語を出力する必用は無い
+                        continue
                 traceable_token = TokenWithOffset(
-                    text=merge_tokens(line_chars[unk_start_token.start_offset:]),
-                    start_line=start_token.start_line,
-                    start_offset=start_token.start_offset,
-                    end_line=end_token.end_line,
-                    end_offset=end_token.end_offset,
+                    text=p,
+                    start_line=line_index,
+                    start_offset=char_index,
+                    end_line=line_index,
+                    end_offset=end_index,
                 )
                 traceable_tokens.append(traceable_token)
-                #logger.debug('line end unk token: %s', traceable_token)
+            pending.clear()
     return traceable_tokens
 
 if __name__ == '__main__':
     DEBUG_MODE = True
-    with open(sys.argv[1], encoding='utf-8') as f:
-        html = f.read()
-        #config_name = 'cl-tohoku/bert-base-japanese-whole-word-masking'
-        config_name = 'cl-tohoku/bert-base-japanese-v2'
-        if len(sys.argv) >= 3:
-            config_name = sys.argv[2]
-        test_tokenizer = AutoTokenizer.from_pretrained(config_name)
-        tokenized = tokenize_with_offsets(test_tokenizer, html)
-        #logger.debug('tokenized: %s', tokenized)
+    #config_name = 'cl-tohoku/bert-base-japanese-whole-word-masking'
+    config_name = 'cl-tohoku/bert-base-japanese-v2'
+    test_tokenizer = AutoTokenizer.from_pretrained(config_name)
+    for path in sys.argv[1:]:
+        logger.debug('testing: %s', path)
+        with open(path, encoding='utf-8') as f:
+            html = f.read()
+            if len(sys.argv) >= 3:
+                config_name = sys.argv[2]
+            tokenized = tokenize_with_offsets(test_tokenizer, html)
+            #logger.debug('tokenized: %s', tokenized)
